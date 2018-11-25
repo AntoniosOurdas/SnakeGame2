aadce9093ce01951a62f0e8791d518a7b26a1177
Tony31614 <45215986+Tony31614@users.noreply.github.com>
2018-11-26 01:12:11 +0200

    Class changes

--- SnakeGame2/src/HeuristicPlayer.java
+++ SnakeGame2/src/HeuristicPlayer.java
@@ -1,25 +1,5 @@
 import java.util.ArrayList;
 
-public class HeuristicPlayer extends Player{
+public class HeuristicPlayer {
 	ArrayList<int[]> path;
-	
-	public HeuristicPlayer(){
-		super();
-	}
-	
-	public HeuristicPlayer(int playerId, int score, String name, Board board){
-		super(playerId, score, name, board);
-	}
-	
-	double evaluate(int currentPos, int dice) {
-		
-	}
-	
-	int getNextMove(int currentPos) {
-		
-	}
-	
-	void statistics() {
-		
-	}
 }
